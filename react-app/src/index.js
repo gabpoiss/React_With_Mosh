cb26f04d841b5f18b60b9cf8abb939b1c975da5c
Gabriel Poissant <poissantgabriel@gmail.com>
2019-01-30 13:43:14 -0500

    Arrow Functions and this

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello World</h1>;
+// const element = < h1 > Hello World < /h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+{ /* ReactDOM.render(element, document.getElementById('root')); */ }
+
+
+const square = number => number * number;
+console.log(square(5));
+
+const jobs = [{
+        id: 1,
+        isActive: true
+    },
+    {
+        id: 2,
+        isActive: true
+    }, {
+        id: 3,
+        isActive: false
+    }
+];
+
+// const activeJobs = jobs.filter(function (job) {
+//     return job.isActive;
+// });
+
+const activeJobs = jobs.filter(job => job.isActive);
+console.log(activeJobs);
