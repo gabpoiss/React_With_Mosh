a91f46ede6dcb3914e1b94177f5909677b9ef1dc
Gabriel Poissant <poissantgabriel@gmail.com>
2019-03-07 13:28:19 -0500

    MoviesTable is now a Class and not a function

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,49 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Like from "./common/like";
 
-const MoviesTable = props => {
-  const { movies, onDelete, onLike, onSort } = props;
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th onClick={() => onSort("title")}>Title</th>
-          <th onClick={() => onSort("genre.name")}>Genre</th>
-          <th onClick={() => onSort("numberInStock")}>Stock</th>
-          <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-          <th onClick={() => onSort("liked")}>Likes</th>
-          <th />
-        </tr>
-      </thead>
+class MoviesTable extends Component {
+  render() {
+    const { movies, onDelete, onLike, onSort } = this.props;
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th onClick={() => onSort("title")}>Title</th>
+            <th onClick={() => onSort("genre.name")}>Genre</th>
+            <th onClick={() => onSort("numberInStock")}>Stock</th>
+            <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
+            <th onClick={() => onSort("liked")}>Likes</th>
+            <th />
+          </tr>
+        </thead>
 
-      <tbody>
-        {movies.map(movie => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like liked={movie.liked} onClick={() => onLike(movie)} />
-              {/* <i
+        <tbody>
+          {movies.map(movie => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <Like liked={movie.liked} onClick={() => onLike(movie)} />
+                {/* <i
       onClick={this.handleLikeClick()}
       className={this.heartBadge()}
       aria-hidden="true"
     /> */}
-            </td>
-            <td>
-              <button
-                onClick={() => onDelete(movie)}
-                className="btn btn-danger btn-sm"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+              </td>
+              <td>
+                <button
+                  onClick={() => onDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default MoviesTable;
